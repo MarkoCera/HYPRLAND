be7baad0c80d613e06fec6ea139ce2ef73d1075f
Marko Cera <markocera2@gmail.com>
2024-12-20 14:31:53 +0100

    added all the dotfiles

---
+++ README.md
@@ -0,0 +1,10 @@
+# Hyprland Dotfiles
+
+## Installation
+
+Clone and run the install script:
+
+```bash
+git clone https://github.com/MarkoCera/HYPRLAND.git
+cd HYPRLAND
+./install.sh
--- install.sh
+++ install.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
+#sudo pacman -S wofi dunst fish fastfetch kitty thunar ranger \
+#    waybar-git
+
+#yay -S vesktop-git spotify ly-git
+
+
+
+
+
+
+
+
 ln -s /home/cera/HYPRLAND/wofi /home/cera/.config/
+ln -s /home/cera/HYPRLAND/dunst /home/cera/.config/
+ln -s /home/cera/HYPRLAND/fish /home/cera/.config/
+ln -s /home/cera/HYPRLAND/fastfetch /home/cera/.config/
+ln -s /home/cera/HYPRLAND/gtk-2.0 /home/cera/.config/
+ln -s /home/cera/HYPRLAND/gtk-3.0 /home/cera/.config/
+ln -s /home/cera/HYPRLAND/kitty /home/cera/.config/
+ln -s /home/cera/HYPRLAND/Thunar /home/cera/.config/
+ln -s /home/cera/HYPRLAND/ranger /home/cera/.config/
+ln -s /home/cera/HYPRLAND/vesktop /home/cera/.config/
+ln -s /home/cera/HYPRLAND/spotify /home/cera/.config/
+ln -s /home/cera/HYPRLAND/waybar /home/cera/.config/
+ln -s /home/cera/HYPRLAND/hypr /home/cera/.config/
+sudo ln -s /home/cera/HYPRLAND/ly /etc/
+sudo rm -rf /home/cera/.config/hypr && ln -sf /home/cera/HYPRLAND/hypr /home/cera/.config
+
+echo "Installation complete! Restart Hyprland for changes to take effect."
+
