652321913dd01a410ff5af886ac335651e791f84
Marko Cera <markocera2@gmail.com>
2025-02-17 01:20:37 +0100

    lists update

--- install.sh
+++ install.sh
@@ -7,31 +7,30 @@ REPO_DIR="$HOME/HYPRLAND"
 LISTS_DIR="$HOME/HYPRLAND/lists"
 
 echo "Installing basic tools..."
-sudo pacman -S --needed --noconfirm git base-devel
+sudo pacman -S --needed --noconfirm git base-devel &> /dev/null
 
 if ! command -v yay &> /dev/null; then
     echo "Installing yay..."
-    git clone https://aur.archlinux.org/yay.git $HOME/yay
-    cd $HOME/yay && makepkg -si --noconfirm
+    git clone -q https://aur.archlinux.org/yay.git $HOME/yay &> /dev/null
+    cd $HOME/yay && makepkg -si --noconfirm &> /dev/null
     cd $HOME && rm -rf $HOME/yay
 fi
 
 echo "Installing pacman packages..."
 if [[ -f "$LISTS_DIR/pkglist.txt" ]]; then
-    sudo pacman -S --needed --noconfirm $(cat lists/pkglist.txt)
+    sudo pacman -S --needed --noconfirm $(cat $LISTS_DIR/pkglist.txt) &> /dev/null
 else
     echo "WARNING: pkglist.txt NOT FOUND!"
 fi
 
 echo "Installing yay packages..."
 if [[ -f "$LISTS_DIR/aur-pkglist.txt" ]]; then
-    yay -S --needed --noconfirm $(cat lists/aur-pkglist.txt)
+    yay -S --needed --noconfirm $(cat $LISTS_DIR/aur-pkglist.txt) &> /dev/null
 else
     echo "WARNING: aur-pkglist.txt NOT FOUND!"
 fi
 
 echo "Creating symbolic links for config files..."
-
 mkdir -p "$CONFIG_DIR"
 for folder in "$REPO_DIR"/*/; do
     folder_name=$(basename "$folder")
@@ -40,30 +39,29 @@ for folder in "$REPO_DIR"/*/; do
         continue
     fi
     
-    rm -rf "$CONFIG_DIR/$folder_name"
-    ln -snf "$folder" "$CONFIG_DIR/$folder_name"
+    rm -rf "$CONFIG_DIR/$folder_name" &> /dev/null
+    ln -snf "$folder" "$CONFIG_DIR/$folder_name" &> /dev/null
 done
 
-sudo ln -snf "$REPO_DIR/ly" /etc/
+sudo ln -snf "$REPO_DIR/ly" /etc/ &> /dev/null
 
-chsh -s /usr/bin/fish
+chsh -s /usr/bin/fish &> /dev/null
 
 echo "Enabling/Starting ly display manager"
-sudo systemctl enable ly.service
-sudo systemctl start ly.service
+sudo systemctl enable ly.service &> /dev/null
+sudo systemctl start ly.service &> /dev/null
 
 echo "Setting up wallpaper,fonts,themes,icons..."
+hyprpaper -i "$REPO_DIR"/hypr/misc/Fantasy-Landscape.png &> /dev/null
 
-hyprpaper -i "$REPO_DIR"/hypr/misc/Fantasy-Landscape.png
-
-gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-pink-standard+default"
+gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-pink-standard+default" &> /dev/null
 if [[ $(gsettings get org.gnome.desktop.interface gtk-theme) != "'catppuccin-mocha-pink-standard+default'" ]]; then
     echo "WARNING: Catppuccin theme might not be installed correctly!"
 fi
 
-gsettings set org.gnome.desktop.interface icon-theme "Tela-pink"
+gsettings set org.gnome.desktop.interface icon-theme "Tela-pink" &> /dev/null
 if [[ $(gsettings get org.gnome.desktop.interface icon-theme) != "'Tela-pink'" ]]; then
-    echo "WARNING: Catppuccin theme might not be installed correctly!"
+    echo "WARNING: Tela-pink icon theme might not be installed correctly!"
 fi
 
 echo "Installation complete! Restart Hyprland for changes to take effect."
